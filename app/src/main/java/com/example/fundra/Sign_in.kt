220a0611bd7d061087e6140f43c829abeb745dda
NadineMahmoud <162837441+NadineMahmoud@users.noreply.github.com>
2025-03-06 02:08:55 +0200

    Initial commit

--- app/src/main/java/com/example/fundra/Sign_in.kt
+++ app/src/main/java/com/example/fundra/Sign_in.kt
@@ -1,20 +1,33 @@
 package com.example.fundra
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fundra.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Sign_in : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signInText.setOnClickListener {
+            startActivity(Intent(this, Sign_up::class.java))
+            finish()
+        }
+        binding.SignInBtn.setOnClickListener{
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
